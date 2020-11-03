32c2221d091f8071a10eca75bf8e27a62d8aed4d
KotaIkehara <ikehara.aurora95@gmail.com>
2020-11-03 20:56:30 +0900

    Finish More Types

---
+++ more-types/slices-pointers.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	names := [4]string{
+		"John",
+		"Paul",
+		"George",
+		"Ringo",
+	}
+	fmt.Println(names)
+
+	a := names[0:2]
+	b := names[1:3]
+	fmt.Println(a, b)
+
+	b[0] = "XXX"
+	fmt.Println(a, b) // b[0]を変えると，a[1]も変わる（スライスは配列への参照だから）
+	fmt.Println(names)
+}
